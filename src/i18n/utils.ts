973cdec70a17653c9935ba8344ce423a3d37485c
Superuser <kevin@e-commetrics.com>
2025-02-06 13:17:43 -0800

    version 1

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,6 +1,11 @@
 import { ui, defaultLang } from "./ui";
 
-export function getLangFromUrl(url: URL) {
+export function getLangFromUrl(url: URL | undefined) {
+  if (!url || !url.pathname) {
+    console.error("Invalid URL object:", url);
+    return defaultLang;
+  }
+
   const [, lang] = url.pathname.split("/");
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
