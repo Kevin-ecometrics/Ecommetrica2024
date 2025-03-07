83a990704ba7f94c49422422ac17a518d262957c
3mmanue1111 <emmanueponcho0015@gmail.com>
2025-03-07 13:29:15 -0800

    actualizacion de esp

--- src/components/team/TeamMembers.js
+++ src/components/team/TeamMembers.js
@@ -18,10 +18,10 @@ const teamMembers = [
     edu2: null,
     testimonio: "Kevin es un Developer excepcional. Su habilidad para estructurar proyectos de manera eficiente con herramientas innovadoras marcó una gran diferencia en nuestro proyecto.",
     testimonio2: "Trabajar con Kevin fue una experiencia increíble. Su dominio web nos permitió desarrollar una aplicación completamente funcional inclusive aun antes del tiempo de entrega. Siempre estuvo disponible para resolver dudas y aportó ideas innovadoras que mejoraron nuestro producto final.",
-    description: "Kevin es un Full-Stack Developer especializado en desarrollo web y aplicaciones móviles, con más de 4 años de experiencia en tecnología y desarrollo digital.",
+    description: "Kevin es un Full-Stack Developer especializado en desarrollo web y aplicaciones móviles, con más de 4 años de experiencia en tecnología y desarrollo digital. Ha trabajado con clientes en el mercado digital, creando soluciones innovadoras y eficientes en el campo educativo, médico y comercio en línea. ",
     extendedDescription: `Kevin Ortega nació en Puebla, pero ha vivido en Tijuana por más de 20 años. Además de su especialización en desarrollo web y aplicaciones móviles, Kevin tiene una pasión por la tecnología y el desarrollo digital. A lo largo de su carrera, ha trabajado en múltiples sectores como educación, salud y comercio en línea.`,
     detailedSkills: `<strong>Lenguajes de programación: </strong> HTML, CSS, JavaScript, TypeScript, React, Vue, Angular, Java, Kotlin, TailwindCSS, MySQL, Firebase, Node.js, Express, Python, Bootstrap, Git.<br><strong>Marketing digital: </strong> Gestión de proyectos y optimización de sitios web con estrategias SEO.<br><strong>Software:</strong> Figma, Trello, Slack, Todoist, MATLAB.<br><strong>SEO:</strong/> Google Search Console, Google Analytics, Pixel, Google Tag Manager.`,
-    workEthic: `Trabajar con Kevin significa contar con alguien apasionado por encontrar soluciones creativas, sin miedo a los retos. Su mentalidad adaptable y pensamiento crítico aportan perspectivas únicas para cada proyecto, llevando a todo el equipo Ecommetrica a nuevas aventuras de innovación y desarrollo.`,
+    workEthic: `Trabajar con Kevin significa contar con alguien apasionado por encontrar soluciones creativas, sin miedo a los retos. Su mentalidad adaptable y pensamiento crítico aportan perspectivas únicas para cada proyecto, llevando a todo el equipo Ecommetrica a nuevas aventuras de innovación y desarrollo.En Ecommetrica, Kevin desempeña un trabajo hábil y ágil, complementando al equipo con su expertise en Front-end web con React.js, Vite.js, Next.js y Astro.js. Además, en Back-end, domina Express, API REST, JWT, OAuth y MySQL. También trabaja en el diseño de interfaces y experiencia de usuario con Figma. Definitivamente, Kevin es un maestro tanto en desarrollo y programación como en planificación y gestión. Su ojo crítico asegura que cada página web esté en las mejores condiciones para destacar en los motores de búsqueda. `,
     additionalDetails: {
       funFacts: [
         "Siempre está al tanto de los últimos lanzamientos tecnológicos.",
@@ -53,8 +53,8 @@ const teamMembers = [
     testimonio2: "Karen es una profesional con un gran ojo para los detalles. Siempre sabe cómo captar la esencia de lo que estamos comunicando y lo presenta de una manera única y efectiva.",
     description: "Es Licenciada en Comunicación y Publicidad, con 5 años de experiencia en escritura creativa, diseño publicitario, eCommerce y edición multimedia.",
     extendedDescription: `Karen es orgullosamente tijuanense y fronteriza, con una visión creativa que transforma ideas en mensajes poderosos. Su ojo crítico y su talento para resignificar y construir mensajes le permiten captar la atención de múltiples audiencias, ya sea B2C, B2B y más.`,
-    detailedSkills: `<strong>Marketing digital: </strong> Gestión de proyectos, SEO, estrategias y planificación de contenido.<br><strong>Software:</strong> Premiere, Audition, CorelDRAW, Todoist, Shopify.<br><strong>Video:</strong> Pre y postproducción.<br><strong>Copywriting:</strong> Creación de mensajes persuasivos y efectivos.`,
-    workEthic: `Trabajar con Karen significa transformar espacios y redefinir la comunicación para crecer. Su enfoque creativo genera un flujo constante de ideas y estrategias para dejar huella en la mente y el corazón del consumidor.`,
+    detailedSkills: `<strong>Marketing digital: </strong> Gestión de proyectos, SEO, estrategias y planificación de contenido.<br><strong>Software:</strong> Premiere, Audition, CorelDRAW, Todoist, Shopify.<br><strong>Video:</strong> Pre y postproducción.<br><strong>Copywriting:</strong> Creación de mensajes persuasivos y efectivos. Ha trabajado en la creación de identidad visual y escrita para los sectores médicos, industrial y comercial. Su experiencia en el comercio en línea y logística le ha permitido aportar ideas innovadoras para transformar mensajes y optimizar la experiencia de negocios. Karen es una fuente confiable para desarrollar textos impactantes y de alto valor, reflejando con precisión la personalidad y el estilo de cada cliente. `,
+    workEthic: `Trabajar con Karen significa transformar espacios y redefinir la comunicación para crecer. Su enfoque creativo genera un flujo constante de ideas y estrategias para dejar huella en la mente y el corazón del consumidor.Es la aliada perfecta para transmitir con claridad y autenticidad todo lo que deseas comunicar con tu marca o negocio.`,
     additionalDetails: {
       funFacts: [
         "Amante de la cultura pop.",
@@ -85,8 +85,8 @@ const teamMembers = [
     testimonio2: "Yazmin tiene un don para el diseño. Su atención al detalle y su capacidad para transformar ideas en interfaces limpias y funcionales realmente marcan la diferencia. Siempre tiene en cuenta la experiencia del usuario, lo que hace que el producto final sea impecable.",
     description: "Es una diseñadora gráfica UX/UI con más de 14 años de experiencia, destacándose por su amplia visión en mercadotecnia y diseño de software.",
     extendedDescription: `Yazmín Guardado, originaria de Tuxtla Gutiérrez, llegó a Tijuana con un talento y carisma extraordinarios. Es una diseñadora UX/UI con más de 14 años de experiencia, destacándose por su amplia visión en mercadotecnia y diseño gráfico. Su creatividad y capacidad de gestión la han convertido en una pieza clave en cada proyecto que toca.`,
-    detailedSkills: `<strong>Marketing digital: </strong> Gestión de proyectos y optimización de sitios web con UX/UI.<br><strong>Software:</strong> Figma, Adobe, Todoist, Shopify.<br><strong>Fotografía:</strong> Profesional (producto y corporativa).<br><strong>Diseño gráfico:</strong> e Ilustración.<br><strong>UX Writing:</strong> Alta capacidad en escritura creativa para mejorar la experiencia del usuario.`,
-    workEthic: `Trabajar con Yaz significa que el cielo no es el límite, sino el comienzo para diseñar sobre él. Yaz es una mujer apasionada por mejorar conceptos, elevar ideas y crear diseños divertidos, adaptables e inclusivos. Es la combinación perfecta entre funcionalidad y estética.`,
+    detailedSkills: `<strong>Marketing digital: </strong> Gestión de proyectos y optimización de sitios web con UX/UI.<br><strong>Software:</strong> Figma, Adobe, Todoist, Shopify.<br><strong>Fotografía:</strong> Profesional (producto y corporativa).<br><strong>Diseño gráfico e Ilustración</strong>.<br><strong>UX Writing:</strong> Alta capacidad en escritura creativa para mejorar la experiencia del usuario.`,
+    workEthic: `Trabajar con Yaz significa que el cielo no es el límite, sino el comienzo para diseñar sobre él. Yaz es una mujer apasionada por mejorar conceptos, elevar ideas y crear diseños divertidos, adaptables e inclusivos. Es la combinación perfecta entre funcionalidad y estética. Ha trabajado en el sector educativo, en la creación de apps y sitios web, además de gestionar redes sociales y ventas online. Su experiencia en el desarrollo de videojuegos le da un enfoque innovador y único, aportando un increíble valor a Ecommetrica. Su trayectoria es rica en referencias, conocimientos y estrategias que elevan cada proyecto al siguiente nivel. Yaz no solo diseña, sino que potencia ideas para hacerlas visualmente impactantes y funcionalmente eficientes. `,
     additionalDetails: {
       funFacts: [
         "Multitasker profesional.",
@@ -116,9 +116,9 @@ const teamMembers = [
     "testimonio": "Manuel González es un consultor estratégico con una profunda comprensión de las plataformas digitales y el comercio electrónico.",
     "testimonio2": "Con su capacidad analítica y su enfoque en la innovación, Manuel ha transformado el comercio electrónico de diversas empresas, siempre buscando un crecimiento sostenible y optimizado.",
     "description": "Manuel González es un experto en ecommerce, con un enfoque innovador y un dominio técnico que le permite desarrollar soluciones avanzadas para el comercio digital.",
-    "extendedDescription": "Manuel González, originario de Tijuana, Baja California, es un líder y consultor en comercio electrónico con más de 10 años de experiencia. Ha trabajado en diversos sectores, ayudando a empresas a implementar soluciones avanzadas y lograr un crecimiento exponencial mediante estrategias de automatización, SEO y análisis de datos.",
-    "detailedSkills": "<strong>Áreas de experiencia:</strong> Ecommerce, Desarrollo web, Análisis de datos, SEO, Growth Hacking.<br><strong>Software:</strong> Shopify, React, R, Python, AgileCRM.<br><strong>Habilidades:</strong> Desarrollo de soluciones ecommerce, análisis de datos, optimización SEO.",
-    "workEthic": "Trabajar con Manuel González significa contar con un líder estratégico que está siempre a la vanguardia, desarrollando soluciones innovadoras y creando un impacto duradero en los negocios.",
+    "extendedDescription": "Manuel González, orgullosamente tijuanense, es el fundador de Ecommetrica y Ecommetrics. Con una mente estratégica y analítica, es un maestro en Economía Aplicada y consultor especializado en comercio electrónico. Con más de 10 años de experiencia en consultoría para negocios digitales y e-commerce, ha trabajado en diversos sectores como marketing digital, agricultura, automatización y logística. Su enfoque ha permitido lograr crecimiento exponencial, la creación de ecosistemas digitales y la implementación de automatización avanzada para mejorar el posicionamiento en Meta y Google. Además, ha desarrollado paneles analíticos en R, programación web en React y tiendas en Shopify con Liquid, integrando múltiples plataformas con AgileCRM y estrategias de growth hacking.",
+    "detailedSkills": "<strong>Áreas de experiencia:</strong> Ecommerce, Desarrollo web, Análisis de datos, SEO, Growth Hacking.<br><strong>Ciencia de datos:</strong> Programación en R y Python.<br><strong>E-commerce:</strong> Desarrollador web y Shopify Expert.<br><strong>Software & Herramientas:</strong> Manejo avanzado de MS Office, G-Suite, Dropbox, Todoist, Trello, Slack.<br><strong>SEO & Redes Sociales:</strong> Optimización de búsqueda, administración de dominios y gestión de campañas. Gestión y programación en Google (G-Suite) y Facebook (Meta).",
+    "workEthic": "Trabajar con Manuel González significa que es un creativo incansable, siempre replanteando y reformulando ideas para generar innovación y una huella digital única. Su pasión por la tecnología y el comercio digital lo impulsa a desarrollar soluciones estratégicas, siempre tres pasos adelante de la competencia.",
     "additionalDetails": {
       "funFacts": [
         "Amante de los viajes.",
@@ -148,9 +148,9 @@ const teamMembers = [
     "testimonio": "Juan Carlos Guerrero es un experto multifacético con una capacidad única para integrar ventas, política y agricultura en su enfoque estratégico.",
     "testimonio2": "Su versatilidad y experiencia le permiten aportar un enfoque integral, ayudando a empresas a alcanzar su máximo potencial en diversas áreas.",
     "description": "Juan Carlos Guerrero es un claro ejemplo de talento transfronterizo, integrando su formación como abogado con una trayectoria multifacética en política, publicidad y creación literaria.",
-    "extendedDescription": "Juan Carlos Guerrero es un consultor estratégico y líder con más de 18 años de experiencia, destacando en ventas, política y agricultura. Su enfoque único le ha permitido impactar diversas industrias, integrando su experiencia transfronteriza para impulsar el crecimiento de sus proyectos.",
-    "detailedSkills": "<strong>Áreas de experiencia:</strong> Ventas, Política, Agricultura, Tecnología.<br><strong>Habilidades:</strong> Estrategia comercial, Comunicación política, Análisis de mercado, Desarrollo en tecnología.",
-    "workEthic": "Trabajar con Juan Carlos significa contar con un experto capaz de integrar distintos sectores, aportando visión y enfoque estratégico para maximizar los resultados.",
+    "extendedDescription": "Originario de San Diego, California. Juan Carlos Guerrero es un claro ejemplo de talento transfronterizo, integrando su formación como abogado con una trayectoria multifacética en política, publicidad y creación literaria. Su versatilidad le ha permitido incursionar en distintos sectores, aportando una visión estratégica y un enfoque innovador en cada uno.",
+    "detailedSkills": "<strong>Ventas y tecnología:</strong> 18 años de experiencia en empresas de EE.UU. y México, desde vendedor de medio tiempo hasta director de ventas.<br><strong>Política:</strong> Más de 20 años en México como operador político y delegado, además de haber sido candidato y coordinador de múltiples campañas.<br><strong>Agricultura:</strong> Más de 20 años en el cultivo comercial, semi-industrial y ornamental en México y EE.UU., combinando producción con su pasión por el campo.",
+    "workEthic": "Porque es mucho más que un vendedor: Su conocimiento en mercados digitales, políticos, culturales y agrícolas le otorga una profunda empatía con clientes y marcas que buscan crecer y expandirse. Trabajar con Juan Carlos significa ir más allá de la meta. Su enfoque es una constante retroalimentación y apoyo, convirtiéndose en el aliado ideal para impulsar cualquier negocio hacia el éxito.",
     "additionalDetails": {
       "funFacts": [
         "Amante de la cocina oriental.",
@@ -170,7 +170,7 @@ const teamMembers = [
     image: "/team6.webp",
     image2: "/team6-2.webp",
     name: "Jesus",
-    fullName: "Jesus González",
+    fullName: "Jesus Castillo",
     role: "Programador Jr.",
     skills: "Desarrollo Web",
     transition: "imagen6",
@@ -180,7 +180,7 @@ const teamMembers = [
     proyecto: "E-commetrica, Programador Jr. y apoyo en desarrollo.",
     proyecto2: "Desarrollo web en proyectos de pequeño y mediano tamaño.",
     proyecto3: null,
-    edu: "Estudios en Ingeniería de Software.",
+    edu: "Estudios en Ingeniería en TICs.",
     edu2: null,
     testimonio: "Jesús es un programador apasionado que ha demostrado una gran capacidad para aprender rápidamente y afrontar retos en el desarrollo web.",
     testimonio2: "A pesar de su corta experiencia, Jesús ha aportado soluciones valiosas a nuestro equipo. Su pasión por la tecnología y la programación se nota en su trabajo.",
@@ -216,14 +216,14 @@ const teamMembers = [
     testimonio: "María José es una editora creativa con un enfoque único que da vida a las ideas con un toque artístico.",
     testimonio2: "Su habilidad para conceptualizar y ejecutar proyectos visualmente atractivos ha elevado nuestro contenido.",
     description: "María José Zuili es una editora con un enfoque artístico y creativo que ha trabajado en diversos proyectos de diseño y marketing.",
-    extendedDescription: `María José Zuili Almanza reside en Querétaro y es una futura arquitecta y artista con un enfoque innovador. Actualmente, forma parte del equipo como Editora, aportando su talento y creatividad a cada proyecto.`,
-    detailedSkills: `<strong>Creación de contenido: </strong> Diseño, conceptualización de planos arquitectónicos y mejora de imágenes.<br><strong>Software:</strong> Adobe Premiere, Lightroom, Illustrator, AutoCAD.<br><strong>Edición y renderizado:</strong> Photoshop, Lightroom.<br><strong>Edición de video:</strong> Postproducción y montaje preciso.`,
-    workEthic: `Trabajar con María José significa dar un sentido artístico y sensible al marketing. Su capacidad para reinventar el contenido y crear nuevas perspectivas en cada imagen y video tiene un impacto en todo el equipo.`,
+    extendedDescription: `María José Zuili Almanza reside en Querétaro y es una futura arquitecta y artista con un enfoque innovador. Actualmente, forma parte del equipo como editora, aportando su talento y creatividad a cada proyecto. Su experiencia se ha centrado en edición y diseño para creadores independientes y empresas en los sectores de arquitectura y marketing. Gracias a su formación académica y su fascinación por el arte, explora constantemente nuevas técnicas y medios para ejecutar cada proyecto con precisión y originalidad.`,
+    detailedSkills: `<strong>Creación de contenido: </strong> Diseño, conceptualización de planos arquitectónicos y mejora de imágenes.<br><strong>Software:</strong> Adobe premier, LightRoom e Ilustrador, AutoCAD. Donde realiza conceptualización de planos y diseños arquitectónicos alineados a las necesidades del cliente.<br><strong>Edición y renderizado:</strong> Mejora de imágenes con horquillado en Photoshop y Lightroom para lograr acabados profesionales.<br><strong>Edición de video:</strong> Postproducción, transiciones dinámicas y montaje preciso.`,
+    workEthic: `Trabajar con María José significa dar un sentido artístico y sensible al marketing. Su capacidad para reinventar lo convencional y generar propuestas únicas y frescas permite que los clientes se diferencien visualmente en sus industrias. Su espíritu artístico abre horizontes para explorar nuevas perspectivas, impulsando el crecimiento de múltiples marcas en diversos sectores. `,
     additionalDetails: {
       funFacts: [
-        "Su amor por la arquitectura la lleva a buscar el equilibrio en todo.",
-        "Una fanática del diseño y la estética.",
-        "Siempre está aprendiendo una nueva técnica de diseño."
+        "Le fascina ilustrar.",
+        "Es muy hábil con las manualidades.",
+        "Ama explorar lugares nuevos."
       ]
     }
   }
